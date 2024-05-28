2aadd716f9d0539f24fedb9fc7ff2206556be4b1
PhigrosX <451464364@qq.com>
2024-05-28 11:09:35 +0800

    upload contracts

---
+++ src/hardhatConfig/contracts/erc721-nft.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// this is the NFT seller contract
+contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    // initial value is zero 
+    uint256 private _nextTokenId;
+
+    constructor() 
+    ERC721("MyNFT", "NFT") 
+    Ownable(msg.sender) {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address to, // the address of the new owner
+        uint256 tokenId, // the token ID of the NFT
+        address auth  // the address of the authorized account
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+    
+    //IERC165
+    //detect wether other contracts have IERC721 receiver for receiving NFTss
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+
+    }
+}
